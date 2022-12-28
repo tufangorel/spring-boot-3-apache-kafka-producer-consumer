e930f844134800743b2cc643bd1b140e911ea87a
tufangorel <tufang@gmail.com>
2022-12-28 18:45:14 +0300

    enable swagger access http://localhost:8081/producer/swagger-ui/index.html

--- kafka-producer-rest-api/src/main/java/com/kafka/producer/api/config/swagger/SwaggerConfig.java
+++ kafka-producer-rest-api/src/main/java/com/kafka/producer/api/config/swagger/SwaggerConfig.java
@@ -1,8 +1,13 @@
 package com.kafka.producer.api.config.swagger;
 
-import org.springdoc.core.GroupedOpenApi;
+
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
@@ -10,9 +15,19 @@ public class SwaggerConfig {
     @Bean
     public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
-                .group("producerapi")
+                .group("producer")
                 .pathsToMatch("/**")
                 .build();
     }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
 
 }
