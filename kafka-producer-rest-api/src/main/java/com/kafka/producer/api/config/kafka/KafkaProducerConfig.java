0fdff8d72380e9327da6c64b2842a3fe335296bf
tufangorel <tufang@gmail.com>
2022-12-31 00:59:31 +0300

    set ProducerListener for kafkaTemplate

--- kafka-producer-rest-api/src/main/java/com/kafka/producer/api/config/kafka/KafkaProducerConfig.java
+++ kafka-producer-rest-api/src/main/java/com/kafka/producer/api/config/kafka/KafkaProducerConfig.java
@@ -1,12 +1,18 @@
 package com.kafka.producer.api.config.kafka;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.ProducerListener;
+import org.springframework.kafka.support.converter.StringJsonMessageConverter;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
@@ -15,23 +21,42 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfig {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducerConfig.class);
+
     @Bean
     public ProducerFactory<String, Object> producerFactory() {
 
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(
-                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "localhost:9092");
-        configProps.put(
-                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-                StringSerializer.class);
+        configProps.put( ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        configProps.put(ProducerConfig.TRANSACTION_TIMEOUT_CONFIG, 1000);
+        //transaction.timeout.ms = 60000
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
     @Bean
-    public KafkaTemplate<String, Object> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
+    KafkaTemplate<String, Object> kafkaTemplate() {
+        KafkaTemplate<String, Object> kafkaTemplate = new KafkaTemplate<>(producerFactory());
+        kafkaTemplate.setMessageConverter(new StringJsonMessageConverter());
+
+        kafkaTemplate.setProducerListener(new ProducerListener<String, Object>() {
+            @Override
+            public void onSuccess(ProducerRecord<String, Object> producerRecord, RecordMetadata recordMetadata) {
+                LOGGER.info("ACK from ProducerListener message: {} offset:  {}",
+                        producerRecord.value().toString(),
+                        recordMetadata.offset());
+            }
+
+            @Override
+            public void onError(ProducerRecord<String, Object> producerRecord, RecordMetadata recordMetadata, Exception exception) {
+                ProducerListener.super.onError(producerRecord, recordMetadata, exception);
+                LOGGER.error("Error from ProducerListener message: {} offset:  {}", producerRecord.value(),
+                        recordMetadata.offset());
+                LOGGER.error("Error from ProducerListener exception : {}",exception.getMessage());
+            }
+        });
+        return kafkaTemplate;
     }
 
 }
