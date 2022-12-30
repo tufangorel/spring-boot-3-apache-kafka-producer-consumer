843d49a70b028f8cbf8958a3b6d56420d9258ec6
tufangorel <tufang@gmail.com>
2022-12-31 00:56:14 +0300

    remove unused logger.

--- kafka-producer-rest-api/src/main/java/com/kafka/producer/api/KafkaProducerRestAPI.java
+++ kafka-producer-rest-api/src/main/java/com/kafka/producer/api/KafkaProducerRestAPI.java
@@ -1,7 +1,5 @@
 package com.kafka.producer.api;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -9,11 +7,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication( exclude = {DataSourceAutoConfiguration.class} )
 public class KafkaProducerRestAPI {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducerRestAPI.class.getName());
-
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaProducerRestAPI.class, args);
-		LOGGER.info("Spring Boot application KafkaProducerRestAPI started!");
 	}
 
 }
