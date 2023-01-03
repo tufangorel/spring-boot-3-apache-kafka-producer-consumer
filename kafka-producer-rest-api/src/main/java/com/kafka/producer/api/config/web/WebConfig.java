4a2ac3a9644d700f3322ad5111d06722632cd03f
tufangorel <tufang@gmail.com>
2023-01-04 01:41:31 +0300

    Remove unnecessary addResourceHandler from WebConfig

--- kafka-producer-rest-api/src/main/java/com/kafka/producer/api/config/web/WebConfig.java
+++ kafka-producer-rest-api/src/main/java/com/kafka/producer/api/config/web/WebConfig.java
@@ -15,9 +15,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
     }
 
     @Override
