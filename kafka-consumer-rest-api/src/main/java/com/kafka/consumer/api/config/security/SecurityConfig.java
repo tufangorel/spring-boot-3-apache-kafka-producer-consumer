68ab05df1ef428cb4fcb956e8964cd3f52ce6f50
tufangorel <tufang@gmail.com>
2023-01-04 01:39:49 +0300

    add @EnableWebSecurity

--- kafka-consumer-rest-api/src/main/java/com/kafka/consumer/api/config/security/SecurityConfig.java
+++ kafka-consumer-rest-api/src/main/java/com/kafka/consumer/api/config/security/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.kafka.consumer.api.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
@@ -12,21 +14,12 @@ public class SecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.csrf().disable();
-        http.headers().frameOptions().disable();
-
-//        return http
-//                .requiresChannel(channel ->
-//                        channel.anyRequest().requiresSecure())
-//                .authorizeRequests(authorize ->
-//                        authorize.anyRequest().permitAll())
-//                .csrf().disable()
-//                .build();
 
         return http
-                .authorizeRequests(authorize ->
-                        authorize.anyRequest().permitAll())
-                .csrf().disable()
+                .authorizeHttpRequests( authorize -> authorize.anyRequest().permitAll() )
                 .build();
     }
+
+
 
 }
