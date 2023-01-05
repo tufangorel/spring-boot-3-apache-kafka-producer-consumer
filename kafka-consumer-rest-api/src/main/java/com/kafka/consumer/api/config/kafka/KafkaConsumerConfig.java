26e9441bacf6038f971fe7f6c94b91532212ee32
tufangorel <tufang@gmail.com>
2023-01-05 23:43:05 +0300

    Register OrderItemDeserializer custom deserializer for kafka consumer config "value.deserializer" .

--- kafka-consumer-rest-api/src/main/java/com/kafka/consumer/api/config/kafka/KafkaConsumerConfig.java
+++ kafka-consumer-rest-api/src/main/java/com/kafka/consumer/api/config/kafka/KafkaConsumerConfig.java
@@ -1,10 +1,50 @@
 package com.kafka.consumer.api.config.kafka;
 
+import com.kafka.consumer.api.config.kafka.deserializer.OrderItemDeserializer;
+import com.kafka.consumer.api.model.OrderItem;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
 @Configuration
 public class KafkaConsumerConfig {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumerConfig.class.getName());
 
+    @Bean
+    public ConsumerFactory<String, OrderItem> consumerFactory() {
+
+        // Creating a map of string-object type
+        Map<String, Object> config = new HashMap<>();
+
+        // Adding the Configuration
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "com.kafka.consumer.api.config.kafka.deserializer.OrderItemDeserializer");
+
+        // Returning message in JSON format
+        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), new OrderItemDeserializer());
+    }
+
+    // Creating a Listener
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, OrderItem> orderItemKafkaListener() {
+        ConcurrentKafkaListenerContainerFactory<String, OrderItem> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+
+        return factory;
+    }
 
 }
