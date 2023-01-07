e7933d2f1444e351d63f802ead50c76883f540f7
tufangorel <tufang@gmail.com>
2023-01-07 17:19:39 +0300

    Add DB Health check config to validate initial database connection.

--- kafka-consumer-rest-api/src/main/java/com/kafka/consumer/api/config/application/listener/ApplicationReadyEventListener.java
+++ kafka-consumer-rest-api/src/main/java/com/kafka/consumer/api/config/application/listener/ApplicationReadyEventListener.java
@@ -1,8 +1,10 @@
 package com.kafka.consumer.api.config.application.listener;
 
+import com.kafka.consumer.api.config.datasource.DBHealthCheckConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Status;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.env.Environment;
@@ -15,10 +17,18 @@ public class ApplicationReadyEventListener implements ApplicationListener<Applic
     private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationReadyEventListener.class);
 
     @Autowired
+    private DBHealthCheckConfig dbHealthCheckConfig;
+
+    @Autowired
     private Environment environment;
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
+        if( dbHealthCheckConfig.health().getStatus() == Status.UP )
+            LOGGER.info("Initial Database Connection Check Success");
+        else
+            LOGGER.info("Initial Database Connection Check Fail");
+
         String port = environment.getProperty("server.port");
         LOGGER.info("Tomcat started on port : "+port);
     }
